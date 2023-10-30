325fca5e2a39500cc52bf1bfef0b90ef70381ea3
Andrei Belskii <mrandrewer@yandex.ru>
2023-10-30 21:09:29 +0300

    Solve fourth task + fix error in test

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -7,4 +7,16 @@ export default class ArraySchema extends BaseSchema {
       this.validators.push((v) => Array.isArray(v));
     }
   }
+
+  maxDepth(depth) {
+    const checkDepth = (arr, d) => {
+        console.log(arr, d);
+        if (!Array.isArray(arr)) return true;
+        if (d === 0) return false;
+        if (arr.length ===0)  return true;
+        return arr.every(a => checkDepth(a, d - 1));
+    };
+    const validator = (v) => checkDepth(v, depth);
+    return new ArraySchema([...this.validators, validator]);
+  }
 }
