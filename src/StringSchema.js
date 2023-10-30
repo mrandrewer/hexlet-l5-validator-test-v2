b9a49b48e73eec834efd6cd0338a0fd2178da9a9
Andrei Belskii <mrandrewer@yandex.ru>
2023-10-30 20:50:58 +0300

    Solve second task

--- src/StringSchema.js
+++ src/StringSchema.js
@@ -7,4 +7,19 @@ export default class StringSchema extends BaseSchema {
       this.validators.push((v) => typeof (v) === 'string');
     }
   }
+
+  startsFromUpperCase() {
+    const validator = (v) => v.length > 0 && v.slice(0, 1).match(/[A-Z]/g) != null;
+    return new StringSchema([...this.validators, validator]);
+  }
+
+  length(len) {
+    const validator = (v) => v.length === len;
+    return new StringSchema([...this.validators, validator]);
+  }
+  
+  hasExclamation() {
+    const validator = (v) => v.match(/[!]/g) != null;
+    return new StringSchema([...this.validators, validator]);
+  }
 }
