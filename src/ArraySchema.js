b39d0a2d7f1e62cc68f75bfaceed8b1db87522c4
Andrei Belskii <mrandrewer@yandex.ru>
2023-10-30 21:10:25 +0300

    Fix linting

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -10,11 +10,11 @@ export default class ArraySchema extends BaseSchema {
 
   maxDepth(depth) {
     const checkDepth = (arr, d) => {
-        console.log(arr, d);
-        if (!Array.isArray(arr)) return true;
-        if (d === 0) return false;
-        if (arr.length ===0)  return true;
-        return arr.every(a => checkDepth(a, d - 1));
+      console.log(arr, d);
+      if (!Array.isArray(arr)) return true;
+      if (d === 0) return false;
+      if (arr.length === 0) return true;
+      return arr.every((a) => checkDepth(a, d - 1));
     };
     const validator = (v) => checkDepth(v, depth);
     return new ArraySchema([...this.validators, validator]);
